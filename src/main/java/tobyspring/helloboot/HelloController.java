d63ad92bf60c4b719972c8cf9c6532d5d2fd7d10
Boseong@0902 <alskdj7879@gmail.com>
2025-09-18 22:56:03 +0900

    컨트롤러 매픙 & 바인딩

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,12 +1,12 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+// 이제 이 클래스에 frontController가 매핑하도록 만들거임
+//@RestController
 public class HelloController {
-    @GetMapping("/hello")
+//    @GetMapping("/hello")
     public String hello(String name){
-        return "Hello" + name;
+        return "AnotherHello" + name;
     }
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -21,19 +21,20 @@ public class HellobootApplication {
 //		SpringApplication.run(HellobootApplication.class, args);
 		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
 		WebServer webServer = factory.getWebServer(new ServletContextInitializer() {
+			HelloController helloController = new HelloController(); // helloController가져와서
 			@Override
 			public void onStartup(ServletContext servletContext) throws ServletException {
 				servletContext.addServlet("frontController", new HttpServlet() { // frontController를 만들어보자
 					@Override
 					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-						// 각 서블릿의 중복 기능들 정의했다고 치고, 기존 컨트롤러가 했던 매핑을 여기서 구현해야함
-						// 요청을 가지고 매핑 - 메서드, 패스, 헤더, 바디 활용
-						if(req.getRequestURI().equals("/hello") && req.getMethod().equals("GET")) { // 즉, hello 패스로 get 요청을 보내면 처리하도록 설정
+						if(req.getRequestURI().equals("/hello") && req.getMethod().equals("GET")) { // hello패스로 get요청 받으면
 						String name = req.getParameter("name");
+
+						String ret = helloController.hello(name); // 리턴값 만들어주고
 
 						resp.setStatus(HttpStatus.OK.value());
 						resp.setHeader("Content-Type", "text/plain");
-						resp.getWriter().println("Hello " + name);
+						resp.getWriter().println(ret); // 리턴값 출력
 						}
 						else if(req.getRequestURI().equals("/users")){
 							//정의
