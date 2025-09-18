44071a3b6c7a2caea74d53f81c4e433b924f3e4b
Boseong@0902 <alskdj7879@gmail.com>
2025-09-18 22:25:42 +0900

    요청, 응답 정의

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -11,6 +11,7 @@ import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 
@@ -25,13 +26,16 @@ public class HellobootApplication {
 				servletContext.addServlet("hello", new HttpServlet() { // 서블릿 이름, 서블릿 객체를 인자로 전달
 					@Override
 					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+						// 요청 제작
+						String name = req.getParameter("name");
+
 						// 응답 제작
 						// 상태 코드
-						resp.setStatus(200);
+						resp.setStatus(HttpStatus.OK.value());
 						// 헤더(컨텐츠 타입 헤더)
 						resp.setHeader("Content-Type", "text/plain");
 						// 바디
-						resp.getWriter().println("Hello Servlet");
+						resp.getWriter().println("Hello " + name);
 					}
 				}).addMapping("/Hello"); // 매핑 패스 지정
 			}
