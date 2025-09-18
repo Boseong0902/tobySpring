fac2f95ff5195615afb69c87b1a921ac17cb289c
Boseong@0902 <alskdj7879@gmail.com>
2025-09-18 22:03:32 +0900

    서블릿 등록

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,18 +2,41 @@ package tobyspring.helloboot;
 
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import java.io.IOException;
 
 //@SpringBootApplication
 public class HellobootApplication {
 	public static void main(String[] args) {
 //		SpringApplication.run(HellobootApplication.class, args);
-		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory(); //TomcatServletWebServer를 생성하는 여러 도구들을 모아놓은 class
-		// 여기서 Tomcat뿐만 아니라 Jetty나 다른 서블릿들도 생성해서 사용할 수 있음 -> 구현되어있
-		WebServer webServer = factory.getWebServer(); //서블릿 컨테이너 생성 메서드
-		webServer.start(); // 톰캣 서블릿 컨테이너 동작
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		WebServer webServer = factory.getWebServer(new ServletContextInitializer() { // 서블릿 등록 익명 클래스
+			@Override
+			public void onStartup(ServletContext servletContext) throws ServletException {
+				servletContext.addServlet("hello", new HttpServlet() { // 서블릿 이름, 서블릿 객체를 인자로 전달
+					@Override
+					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+						// 응답 제작
+						// 상태 코드
+						resp.setStatus(200);
+						// 헤더(컨텐츠 타입 헤더)
+						resp.setHeader("Content-Type", "text/plain");
+						// 바디
+						resp.getWriter().println("Hello Servlet");
+					}
+				}).addMapping("/Hello"); // 매핑 패스 지정
+			}
+		});
+		webServer.start();
 	}
 
 }
